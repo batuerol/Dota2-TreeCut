8d45142cb125d79db1129bb13684b9b64e259ba8
mindfog <mindfog>
2016-05-20 23:05:44 +0300

    Well... If we don't move the cursor we actually cut LOTS of trees so this seems fine.

--- CutCutCut-Dota2/main.cpp
+++ CutCutCut-Dota2/main.cpp
@@ -2,10 +2,25 @@
 #define WIN32_LEAN_AND_MEAN
 
 #include <iostream>
+#include <cmath>
+#include <thread>
 
 //static const char* g_TargetWindow = "Dota 2";
+static const float TWO_TIMES_PI = 3.14159265358979323846 * 2;
 static bool g_Running = true;
 static HWND g_TargetWindow;
+static RECT g_DotaRect;
+
+void MoveMouse(int x, int y)
+{
+	INPUT input = { 0 };
+	input.type = INPUT_MOUSE;
+	input.mi.dx = (g_DotaRect.left + x) * (0xFFFF / (g_DotaRect.right - g_DotaRect.left));
+	input.mi.dy = (g_DotaRect.top + y) * (0xFFFF / (g_DotaRect.bottom - g_DotaRect.top));
+	input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;// | MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_RIGHTDOWN;
+	
+	SendInput(1, &input, sizeof(INPUT));
+}
 
 void SendClick()
 {
@@ -13,9 +28,17 @@ void SendClick()
 	GetCursorPos(&p);
 	LPARAM lParam = MAKELPARAM(p.x, p.y);
 	SendMessage(g_TargetWindow, WM_LBUTTONDOWN, MK_LBUTTON, lParam);
-	SendMessage(g_TargetWindow, WM_LBUTTONUP, MK_LBUTTON, lParam);
 	SendMessage(g_TargetWindow, WM_RBUTTONDOWN, MK_RBUTTON, lParam);
+	SendMessage(g_TargetWindow, WM_LBUTTONUP, MK_LBUTTON, lParam);
 	SendMessage(g_TargetWindow, WM_RBUTTONUP, MK_RBUTTON, lParam);
+}
+
+void ClickerThreadFunc()
+{
+	while(g_Running)
+	{
+		SendClick();
+	}
 }
 
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
@@ -24,8 +47,9 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 	if (kbStruct->vkCode == VK_ESCAPE && wParam == WM_KEYUP)
 	{
 		g_Running = false;
+		std::cout << "TRIGGERED" << std::endl;
 	}
-	
+
 	return 0;
 }
 
@@ -100,7 +124,11 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	}
 
 	g_TargetWindow = FindWindow(NULL, "Dota 2");
+	GetClientRect(g_TargetWindow, &g_DotaRect);		
+	SetForegroundWindow(g_TargetWindow);
+	Sleep(100);
 
+	//std::thread clicker(ClickerThreadFunc);
 	MSG message;
 	while (g_Running)
 	{
@@ -109,8 +137,39 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 			TranslateMessage(&message);
 			DispatchMessage(&message);
 		}
-		SendClick();
+
+		// TODO(batuhan): Clicker thread		
+		MoveMouse(476, 705);
+		SendClick();		
+
+		const int a = 767;
+		const int b = 515;
+		const int r = 241;		
+		//for (float i = 0.0f; i < TWO_TIMES_PI; i += 0.25f)
+		//{
+		//	int x = a + (r * cos(i));
+		//	int y = b + (r * sin(i));
+		//	MoveMouse(x, y);
+		//	SendClick();
+		//}
+
+		const int left = 295;
+		const int top = 268;
+		const int right = 1260;
+		const int bottom = 865;
+		
+		//for (int x = left; x <= right; x += 30)
+		//{
+		//	for (int y = top; y <= bottom; y += 50)
+		//	{
+		//		MoveMouse(x, y);
+		//		SendClick();
+		//	}
+		//}
+		//Sleep(1000);
 	}
+	
+	//clicker.join();
 
 	return 0;
 }
